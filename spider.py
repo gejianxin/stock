e85534a622fac52c675d953ff9d25648d4151d32
Ge Jianxin <gejianxin@yahoo.com>
2020-12-20 21:42:05 +0800

    Basic spider function to crawl down data.

---
+++ spider.py
@@ -0,0 +1,10 @@
+# -*- coding: UTF-8 -*-
+
+import stock
+
+
+stock.getStock(path='C:\\Users\\User\\Documents\\data',
+               symbol='sh600000',
+               start_date='2020-11-01',
+               end_date='2020-12-25',
+               adjust='qfq')
