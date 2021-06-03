38c811cea31a808284c13cbff1a9d105bd51d49b
Ge Jianxin <gejianxin@yahoo.com>
2021-06-03 23:09:19 +0800

    Add indicator.

--- Indicator.py
+++ Indicator.py
@@ -2,3 +2,9 @@ import backtrader as bt
 
 
 class Ketler(bt.Indicator):
+    lines = dict(upper='upperline',
+                 middle='middleline',
+                 lower='lowerline')
+    params = dict(ema=20, atr=17)
+    plotinfo = dict(subplot=False)
+    plotlines = dict()
