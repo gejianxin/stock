7d7de94bddca2696ae70d8f26e91b111458c33d3
Ge Jianxin <gejianxin@yahoo.com>
2021-06-17 23:35:51 +0800

    Minor fixes.

--- strategy.py
+++ strategy.py
@@ -8,7 +8,7 @@ from indicator import Ketler, HMA, POLY
 import backtrader.analyzers as btanalyzers
 
 
-class MyStrategy(bt.Strategy):
+class KetlerStrategy(bt.Strategy):
     def log(self, txt, dt=None):
         ''' Logging function for this strategy'''
         dt = dt or self.datas[0].datetime.date(0)
@@ -17,9 +17,6 @@ class MyStrategy(bt.Strategy):
     def __init__(self):
         self.close = self.datas[0].close
         self.ketler = Ketler()
-        # self.hma = bt.indicators.HullMovingAverage(period=10)
-        self.hma = HMA()
-        self.poly = POLY()
 
     def notify_order(self, order):
         if order.status in [order.Submitted, order.Accepted]:
@@ -71,6 +68,61 @@ class MyStrategy(bt.Strategy):
         else:
             if self.close[0] < self.ketler.upper[0]:
                 self.order = self.sell()
+
+
+class PolyHmaStrategy(bt.strategy):
+    def log(self, txt, dt=None):
+        ''' Logging function for this strategy'''
+        dt = dt or self.datas[0].datetime.date(0)
+        print('%s, %s' % (dt.isoformat(), txt))
+
+    def __init__(self):
+        self.close = self.datas[0].close
+        self.hma = HMA()
+        self.poly = POLY()
+
+    def notify_order(self, order):
+        if order.status in [order.Submitted, order.Accepted]:
+            # Buy/Sell order submitted/accepted to/by broker - Nothing to do
+            return
+
+        # Check if an order has been completed
+        # Attention: broker could reject order if not enough cash
+        if order.status in [order.Completed]:
+            if order.isbuy():
+                self.log(
+                    '【开仓】  价格： {:.2f}  数量： {:d}  总价： {:.2f}  佣金： {:.2f}'.
+                    format(
+                        order.executed.price,
+                        order.size,
+                        order.executed.value,
+                        order.executed.comm
+                        ))
+                self.buyprice = order.executed.price
+                self.buycomm = order.executed.comm
+            elif order.issell():
+                self.log(
+                    '【平仓】  价格： {:.2f}  数量： {:d}  总价： {:.2f}  佣金： {:.2f}'.
+                    format(
+                        order.executed.price,
+                        order.size,
+                        order.executed.value,
+                        order.executed.comm
+                        ))
+
+            # self.bar_executed = len(self)
+
+        elif order.status in [order.Canceled, order.Margin, order.Rejected]:
+            self.log('Order Canceled/Margin/Rejected')
+
+        self.order = None
+
+        def notify_trade(self, trade):
+            if not trade.isclosed:
+                pass
+            else:
+                self.log('【单笔交易盈利】  毛利： {:.2f}  净利： {:.2f}'.format(
+                    trade.pnl, trade.pnlcomm))
 
 
 if __name__ == '__main__':
