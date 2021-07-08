7397c498164d143d552dc2b757dadba0303fe880
Ge Jianxin <gejianxin@yahoo.com>
2021-07-08 17:09:34 +0800

    Add database operation function.

--- tools/data.py
+++ tools/data.py
@@ -1,11 +1,11 @@
-import backtrader as bt
-import backtrader.feeds as btfeeds
 from datetime import datetime as dt, timedelta
 from datetime import date
+import backtrader as bt
+import backtrader.feeds as btfeeds
 import psycopg2
-from psycopg2 import Error
+from psycopg2 import Error, sql
 import tushare as ts
-from ..config.essential import TOKEN, YEARS
+from ..config.essential import DB, TOKEN, YEARS
 
 
 # docker run command
@@ -23,60 +23,101 @@ def get_all_tickers(token=TOKEN):
 
 
 def get_online_data(ticker, fromdate, todate):
-    data = btfeeds.YahooFinanceData(dataname=ticker,
-                                    fromdate=dt.strptime(fromdate, '%Y-%m-%d'),
-                                    todate=dt.strptime(todate, '%Y-%m-%d'),
-                                    timeframe=bt.TimeFrame.Days
-                                    )
+    if isinstance(fromdate, date):
+        try:
+            fromdate=dt.strptime(fromdate, '%Y-%m-%d')
+        except ValueError as error:
+            template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+            message = template.format(type(error).__name__, error.args)
+            print (message)
+    if isinstance(todate, date):
+        try:
+            todate=dt.strptime(todate, '%Y-%m-%d')
+        except ValueError as error:
+            template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+            message = template.format(type(error).__name__, error.args)
+            print (message)
+
+    data = btfeeds.YahooFinanceData(dataname=ticker, fromdate=fromdate, todate=todate, timeframe=bt.TimeFrame.Days)
     return data
 
 
+def update_db_data(ticker, fromdate, todate=dt.now().date(), db=DB):
+    if isinstance(fromdate, date):
+        try:
+            fromdate=dt.strptime(fromdate, '%Y-%m-%d')
+        except ValueError as error:
+            template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+            message = template.format(type(error).__name__, error.args)
+            print (message)
+
+    records = get_db_data(ticker=ticker, fromdate=fromdate, todate=todate, db=db)
+    firstdate = dt.strptime(records['date', 0], '%Y-%m-%d')
+    lastdate = dt.strptime(records['date', -1], '%Y-%m-%d')
+    delta = (fromdate - firstdate).days
+    if delta < 0:  # fromdate < firstday, the database need to fullfil data before firstdate
+        data = get_online_data(ticker=ticker, fromdate=fromdate, todate=firstdate-timedelta(days=1))
+        insert_db_data(ticker=ticker, data=data, db=db)  # insert data into db
+    delta = (todate - lastdate).days
+    if delta > 0:  # todate > lastday, the database need to fullfil data after lastday
+        data = get_online_data(ticker, fromdate=lastdate+timedelta(days=1), todate=todate)
+        insert_db_data(ticker=ticker, data=data, db=db)  # insert data into db
+
+
 # default update recent 10 years data
-def update_all_data(ticker, year=YEARS):
-    # TODO: check database firstday lastday
-    # Beaware firstday and lastday datatype, if not is date, you need to covert it to date object
+def update_all_db_data(ticker, year=YEARS, db=DB):
+    # Check database firstday lastday
     todate = dt.now().date()
     fromdate = todate - timedelta(days=365.25*year)
+    records = get_db_data(ticker=ticker, fromdate=fromdate, todate=todate, db=db)
+    firstdate = dt.strptime(records['date', 0], '%Y-%m-%d')
+    lastdate = dt.strptime(records['date', -1], '%Y-%m-%d')
     # check if firstday was before fromdate
-    delta = fromdate - firstday
+    delta = (fromdate - firstdate).days
     if delta < 0:  # fromdate < firstday, the database need to fullfil data before firstdate
-        data = get_online_data(ticker, fromdate, todate=firstdate-timedelta(days=1))
-        # TODO: insert data into db
-    delta = todate - lastday
+        data = get_online_data(ticker=ticker, fromdate=fromdate, todate=firstdate-timedelta(days=1))
+        insert_db_data(ticker=ticker, data=data, db=db)  # insert data into db
+    delta = (todate - lastdate).days
     if delta > 0:  # todate > lastday, the database need to fullfil data after lastday
-        data = get_online_data(ticker, fromdate=lastday+timedelta(days=1), todate=todate)
-        # TODO: insert data into db
-
-
-def update_data(ticker, fromdate, todate):
-    # TODO: check database firstday lastday
-    # Beaware firstday and lastday datatype, if not is date, you need to covert it to date object
-    fromdate = dt.strptime(fromdate, '%Y-%m-%d')
-    todate = dt.strptime(todate, '%Y-%m-%d')
-    # check if firstday was before fromdate
-    delta = fromdate.date() - firstday
-    if delta < 0:  # fromdate < firstday, the database need to fullfil data before firstdate
-        data = get_online_data(ticker, fromdate, todate=firstdate-timedelta(days=1))
-        # TODO: insert data into db
-    delta = todate.date() - lastday
-    if delta > 0:  # todate > lastday, the database need to fullfil data after lastday
-        data = get_online_data(ticker, fromdate=lastday+timedelta(days=1), todate=todate)
-        # TODO: insert data into db
+        data = get_online_data(ticker, fromdate=lastdate+timedelta(days=1), todate=todate)
+        insert_db_data(ticker=ticker, data=data, db=db)  # insert data into db
 
 
 def get_csv_data(pathname, fromdate, todate):
+    if isinstance(fromdate, date):
+        try:
+            fromdate=dt.strptime(fromdate, '%Y-%m-%d')
+        except ValueError as error:
+            template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+            message = template.format(type(error).__name__, error.args)
+            print (message)
+    if isinstance(todate, date):
+        try:
+            todate=dt.strptime(todate, '%Y-%m-%d')
+        except ValueError as error:
+            template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+            message = template.format(type(error).__name__, error.args)
+            print (message)
     data = btfeeds.YahooFinanceCSVData(
         dataname=pathname,
         # Do not pass values before this date
-        fromdate=dt.strptime(fromdate, '%Y-%m-%d'),
+        fromdate=fromdate,
         # Do not pass values before this date
-        todate=dt.strptime(todate, '%Y-%m-%d'),
+        todate=todate,
         # Do not pass values after this date
         reverse=False)
     return data
 
 
-def get_db_data(db, ticker):
+def get_db_data(ticker, fromdate, todate=dt.now().date(), db=DB):
+    if isinstance(fromdate, date):
+        try:
+            fromdate=dt.strptime(fromdate, '%Y-%m-%d')
+        except ValueError as error:
+            template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+            message = template.format(type(error).__name__, error.args)
+            print (message)
+
     try:
         # Connect to an existing database
         connection = psycopg2.connect(user=db.username,
@@ -87,19 +128,45 @@ def get_db_data(db, ticker):
 
         # Create a cursor to perform database operations
         cursor = connection.cursor()
-        # Print PostgreSQL details
-        # print("PostgreSQL server information")
-        # print(connection.get_dsn_parameters(), "\n")
-        # Executing a SQL query
-        # cursor.execute("SELECT version();")
-        # Fetch result
-        # record = cursor.fetchone()
-        # print("You are connected to - ", record, "\n")
+        query = sql.SQL('SELECT * FROM {table} WHERE DATE BETWEEN {fromdate} TO {todate} ASC').format(table=sql.Identifier(ticker), fromdate=sql.Identifier(fromdate), todate=sql.Identifier(todate))
+        cursor.execute(query)
+        records = cursor.fetchall()
+        return records
 
     except (Exception, Error) as error:
         print("Error while connecting to PostgreSQL", error)
     finally:
-        if (connection):
-            cursor.close()
-            connection.close()
-            print("PostgreSQL connection is closed")
+        cursor.close()
+        connection.close()
+        print("PostgreSQL connection is closed")
+
+
+def insert_db_data(ticker, data, db=DB):
+    try:
+        # Connect to an existing database
+        connection = psycopg2.connect(user=db.username,
+                                      # password="123456",
+                                      host=db.host,
+                                      port=db.port,
+                                      database=db.database)
+
+        # Create a cursor to perform database operations
+        cursor = connection.cursor()
+
+        if isinstance(data, list):
+            try:
+                data=data.values.tolist()
+            except ValueError as error:
+                template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+                message = template.format(type(error).__name__, error.args)
+                print (message)
+
+        query = sql.SQL('INSERT INTO {table} VALUES (%s, %f, %f, %f, %f, %f)').format(table=sql.Identifier(ticker))
+        cursor.executemany(query, data)
+        cursor.commit()
+
+    except (Exception, Error) as error:
+        print("Error while connecting to PostgreSQL", error)
+    finally:
+        cursor.close()
+        connection.close()
