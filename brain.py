3c407126a39ef714a54447ad1821a52e15177003
Ge Jianxin <gejianxin@yahoo.com>
2021-10-22 17:46:56 +0800

    2021-10-22

--- brain.py
+++ brain.py
@@ -1,15 +1,17 @@
-from baostock.login.loginout import login
+import math
+# from backtrader import position, signal
+# from baostock.login.loginout import login
+from datetime import datetime as dt
+import requests
+# import json
 import numpy as np
 import pandas as pd
 import backtrader as bt
-from datetime import datetime as dt
-import requests
-import json
 from fake_headers import Headers
 import baostock as bs
 
 
-def get_s2n_bar():
+def get_s2n_bar(end_date=dt.today().strftime('%Y-%m-%d')):
     login = bs.login()
     # 显示登陆返回信息
     print('login respond error_code:'+login.error_code)
@@ -21,7 +23,7 @@ def get_s2n_bar():
     # 2018年4月11日，证监会同意将沪股通和深股通每日额度调整为520亿元人民币，自2018年5月1日起生效。
     # 返回记录有两列 calendar_date & is_trading_day
     # is_trading_day 值为 0 或 1，字符类型
-    records = bs.query_trade_dates(start_date='2014-11-17', end_date=dt.today().strftime('%Y-%m-%d'))
+    records = bs.query_trade_dates(start_date='2014-11-17', end_date=end_date)
     print('query_trade_dates respond error_code:'+records.error_code)
     print('query_trade_dates respond  error_msg:'+records.error_msg)
     result = []
@@ -35,8 +37,6 @@ def get_s2n_bar():
 
     return len(result)
 
-class S2N():
-    data = get_s2n_bar()
 
 def choose_time():
     # 根据北向资金选择入市时机
@@ -48,11 +48,16 @@ def choose_time():
     # params: lmt, 获取 lmt=xxx 时间单位长度数据
     # return: data 数据集合，包含后面几项； hk2sh 沪港通； hk2sz 深港通； s2n 北向资金
     # '''
-    bar = S2N.data # 数据长度
-    URL = 'https://push2his.eastmoney.com/api/qt/kamt.kline/get?fields1=f1,f3,f5&fields2=f51,f52&klt=101&lmt='+bar
+    bar_length = get_s2n_bar() # 数据长度
+    URL = 'https://push2his.eastmoney.com/api/qt/kamt.kline/get?fields1=f1,f3,f5&fields2=f51,f52&klt=101&lmt='+str(bar_length) # 字符串连接必须把 int 转换为 string
     headers = Headers(os="win", headers=True).generate()
     response = requests.get(URL, headers=headers)
     records = response.json() if response and response.status_code == 200 else None
+
+    if records is None:
+        print('No records or retrive error!')
+        return
+
     records = records['data']['s2n']
     result = []
     for record in records:
@@ -71,15 +76,56 @@ def choose_time():
     # 2021-08-26   527087.19
     # 2021-08-27  1022954.70
 
-    money
+    up = []
+    down = []
     for i in range(3, bar):
+        today_data = result.iloc[0:i+1,:]
+        today_data = today_data.sort_values(by='net_flowin', ascending=False)
+        row_num = today_data.shape[0]
+        up_index = math.ceil(row_num/3) - 1
+        down_index = math.ceil(row_num*2/3) - 1
+        up_value = today_data.iloc[up_index].values[0]
+        down_value = today_data.iloc[down_index].values[0]
+        up.append(up_value)
+        down.append(down_value)
+
+    threshold_dict = {'up_value':up, 'down_value':down}
+    threshold_df = pd.DataFrame(threshold_dict, index=result.index[3:])
+    # print(threshold_df)
+    #              up_value  down_value
+    # date                             
+    # 2021-10-11  358482.75   230902.23
+    # 2021-10-12  358482.75   108882.86
+    # 2021-10-15  460719.94   230902.23
+    # 2021-10-18  358482.75   108882.86
+    # 2021-10-19  460719.94   108882.86
+    # 2021-10-20  654660.67   230902.23
+    # 2021-10-21  654660.67   230902.23
+    signal_df = pd.concat([threshold_df, result.iloc[3:]], axis=1)
+    # print(signal_df)
+    #              up_value  down_value  net_flowin
+    # date                                         
+    # 2021-10-11  358482.75   230902.23   358482.75
+    # 2021-10-12  358482.75   108882.86    87660.56
+    # 2021-10-15  460719.94   230902.23   654660.67
+    # 2021-10-18  358482.75   108882.86  -442404.85
+    # 2021-10-19  460719.94   108882.86   737883.57
+    # 2021-10-20  654660.67   230902.23   761718.27
+    # 2021-10-21  654660.67   230902.23  1375718.00
+
+    return threshold_df
 
 
-    pass
+def s2n_signal():
+    threshold_df = choose_time()
+    signal = [] # signal has 3 values 1/0/-1, represent bigger/.../smaller than up_value/.../down_value
+    for i in range(len(threshold_df)):
+        if threshold_df['net_flowin'][i] >= threshold_df['up_value'][i]:
+            signal.append(1)
+        elif threshold_df['net_flowin'][i] < threshold_df['down_value'][i]:
+            signal.append(-1)
+        else:
+            signal.append(0)
+    signal_df = pd.DataFrame(signal, index=threshold_df.index)
 
-
-def choose_ticker(number=1):
-    # Calculate all tickers next 
-
-if __name__ == '__main__':
-    # Choose right time to trade
+    return signal_df
--- tools/proxy.py
+++ tools/proxy.py
@@ -1,20 +1,42 @@
+import random
+import time
 import requests
 from bs4 import BeautifulSoup
 from fake_headers import Headers
 
 
-def get_proxy(BASEURL, URL, proxies):
+def get_proxy(URL='http://www.xiladaili.com/http'):
     headers = Headers(os="win", headers=True).generate()
-    response = requests.get(URL, headers=headers)
-    soup = BeautifulSoup(response.text, features='lxml')
-    table = soup.select('body > div:nth-child(4) > div.bl > div.data > table > tr')
-    for row in table:
-        ip = row.select('tr > td:nth-child(1)')[0]
-        port = row.select('tr > td:nth-child(4) > span')[0]
-        proxies.append(':'.join([ip.string, port.string]))
+    proxies = []
 
-    page_ref = soup.select('body > div:nth-child(4) > div.bl > div > table > tfoot > tr > td > div > a')
-    if len(page_ref) > 1:
-        URL = BASEURL + page_ref[1]['href']
-        get_proxy(BASEURL, URL, proxies)
+    for i in range(1, 50):
+        response = requests.get("{url}/{i}".format(url=URL, i=i), headers=headers)
+        time.sleep(random.uniform(0.1, 2.2))
+
+        response = requests.get(URL, headers=headers)
+        soup = BeautifulSoup(response.text, features='lxml')
+        tr = soup.select('body > div > div > div > table > tbody > tr')
+        for td in tr:
+            if '高匿' in td.select('tr > td:nth-child(3)')[0].text and '天' in td.select('tr > td:nth-child(6)')[0].text:
+                proxy = td.select('tr > td:nth-child(1)')[0].text
+                proxies.append(proxy)
+    print(proxies)
     return proxies
+
+def check_proxy(proxy, CHECK_URL='http://icanhazip.com'):
+    headers = Headers(os="win", headers=True).generate()
+    proxy_dict = {'https': 'https://' + proxy}
+
+    try:
+        time.sleep(1)
+        # 发送测试请求
+        response = requests.get(CHECK_URL, headers=headers, proxies=proxy_dict, timeout=1)
+        if response.status_code == 200:
+            print('有效IP: ' + proxy)
+            with open('xila_https_list.txt', 'a') as f:
+                f.write(proxy)
+                f.write('\n')
+        else:
+            print('无效IP: ' + proxy)
+    except requests.HTTPError as e:
+        print('Http Error: ' + e + ' 无效IP: ' + proxy)
